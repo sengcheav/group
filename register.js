6543a2a79141488ea314783986426241a60ed450
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-30 16:09:06 +1200

    f

---
+++ register.js
@@ -0,0 +1,38 @@
+
+// use the express middleware
+var express = require('express');
+
+// make express handle JSON and other requests
+var bodyParser = require('body-parser');
+
+// use cross origin resource sharing
+var cors = require('cors');
+
+// instantiate app
+var app = express()
+  , pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+
+// make sure we can parse JSON
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
+
+// make sure we use CORS to avoid cross domain problems
+app.use(cors());
+
+app.get('/' , function(req, res){
+        res.sendfile('index.html');
+
+});
+
+// use PORT set as an environment variable
+var server = app.listen(process.env.PORT, function() {
+    console.log('Listening on port %d', server.address().port);
+                                                                
---
+++ schema.js
@@ -0,0 +1,26 @@
+
+var pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL
+  , client
+  , query ;
+
+var usr = [
+        {username : 'seng', password : 'invalid'},
+        {username : 'lol', password : 'invalid'},
+        {username : 'haha', password : 'invalid'},
+        {username : 'yobro', password : 'invalid'}
+
+];
+
+client = new pg.Client(connectionString);
+client.connect();
+//query = client.query ('drop table quote');
+query = client.query('CREATE TABLE user(username varchar(15) primary key , password varchar(15)  , id serial , boolean login default false );
+for ( var id = 0 ; id <4 ; id++){
+
+query = client.query('INSERT INTO user (username , password) VALUES($1, $2)', [usr[id].username , usr[id].password]);
+}
+query.on('end', function(result) { client.end(); });
+
+
+
