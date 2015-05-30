da5cf5f0647d0992d9b520acd89113ad60e22520
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-30 16:15:17 +1200

    query create editing

--- schema.js
+++ schema.js
@@ -15,7 +15,8 @@ var usr = [
 client = new pg.Client(connectionString);
 client.connect();
 //query = client.query ('drop table quote');
-query = client.query('CREATE TABLE user(username varchar(15) primary key , password varchar(15)  , id serial , boolean login default false );
+//query = client.query('CREATE TABLE user(username varchar(15) primary key , password varchar(15)  , id serial , boolean login default false' );
+query = client.query ('CREATE TABLE USER (USERNAME VARCHAR(15) PRIMARY KEY , PASSWORD VARCHAR(15) , ID SERIAL UNIQUE , BOOLEAN LOGIN DEFAULT FALSE'); 
 for ( var id = 0 ; id <4 ; id++){
 
 query = client.query('INSERT INTO user (username , password) VALUES($1, $2)', [usr[id].username , usr[id].password]);
