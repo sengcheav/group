a64f15018e8af2b978cdb10c43ed95257cb8f326
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-30 16:29:00 +1200

    userlogin table

--- schema.js
+++ schema.js
@@ -14,10 +14,10 @@ var usr = [
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query ('CREATE TABLE user (USERNAME VARCHAR(15) PRIMARY KEY , PASSWORD VARCHAR(15) , ID SERIAL UNIQUE , BOOLEAN LOGIN DEFAULT FALSE)'); 
+query = client.query ('CREATE TABLE userlogin ( USERNAME VARCHAR(15) PRIMARY KEY , PASSWORD VARCHAR(15) , ID SERIAL UNIQUE ,  LOGIN BOOLEAN DEFAULT FALSE)'); 
 for ( var id = 0 ; id <4 ; id++){
 
-query = client.query('INSERT INTO user (username , password) VALUES($1, $2)', [usr[id].username , usr[id].password]);
+query = client.query('INSERT INTO userlogin (username , password) VALUES($1, $2)', [usr[id].username , usr[id].password]);
 }
 query.on('end', function(result) { client.end(); });
 
