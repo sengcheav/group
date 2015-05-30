73dfdf1c60d4a2a86f3449925b09b7f7a78fe0f5
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-30 20:20:45 +1200

    hash and salt

--- register.js
+++ register.js
@@ -33,7 +33,7 @@ app.get('/' , function(req, res){
 passwordHash('mysecret').hash(function(error, hash) {
 if(error)
         throw new Error('Something went wrong!');
-console.log(hash) ;
+   console.log("This is hash " + hash) ;
   res.sendfile('index.html');
 
 
