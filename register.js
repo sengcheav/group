271401c50a355ab50829f219a2e429b0997907d1
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-20 00:55:53 +1200

    syntax

--- register.js
+++ register.js
@@ -49,7 +49,7 @@ query  = client.query('SELECT POINTS_LVL [$1 ] AS points FROM RANK WHERE usernam
 if(err) {console.log(err.message) ; res.send("errror");}
 if (!result) { console.log ( "NOT FOUND ") ; res.statusCode = 404 ; res.send("404: NOT FOUND") ;}
 else {
-console.log("Suceess : Point at lvl" + req.params.lvl + " : "+ result.points) ;
+console.log("Suceess : Point at lvl" + req.params.lvl + " : "+ result) ;
 res.statusCode = 200 ;  
 res.send(result.point) ;
 }  
