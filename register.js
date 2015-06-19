ce02e58dc1e4a5c08a47b291143e42112dcf730f
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-19 23:09:49 +1200

    g

--- register.js
+++ register.js
@@ -47,9 +47,10 @@ point : req.body.point,
  
 query  = client.query('SELECT POINTS_LVL [$1 ] AS points FROM RANK WHERE username = $2' ,[req.params.lvl , obj.username]  ) 
 query.on('row', function (result){
-if (!result) { console.log ( "NOT FOUND ") ; res.send("404: NOT FOUND") ;}
+if (!result) { console.log ( "NOT FOUND ") ; res.statusCode = 404 ; res.send("404: NOT FOUND") ;}
 else {
-console.log("Point at lvl" + req.params.lvl + " : "+ result.points) ; 
+console.log("Suceess : Point at lvl" + req.params.lvl + " : "+ result.points) ; 
+res.statusCode = 200 ;
 res.send(result.point) ;	
 } 
 
