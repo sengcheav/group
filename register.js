cb315323219573aabe007f39de3b4aee769186dc
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-19 17:55:53 +1200

    update

--- register.js
+++ register.js
@@ -68,7 +68,7 @@ query.on("row", function(result){
  if(!result){console.log("!result") ;  res.send("NO"); }
  else {
     if(result.best < point) { console.log("result.best < point") ; 
-       client.query ('UPDATE rank SET points_lvl[$1] = $2, lvl_best[$1]= $2, totalpoints += $3',[req.params.lvl, point,(point-result.best)], funtion(err){
+       client.query ('UPDATE rank SET points_lvl[$1] = $2, lvl_best[$1]= $2, totalpoints += $3',[req.params.lvl, point,(point-result.best) ], function(err){
        if(err){console.log(err.message) ; res.send(err.message) ;}	
        console.log("updated"); 
        res.send( 'UPDATED' ) ;	
