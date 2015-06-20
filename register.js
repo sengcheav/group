7843a17f741b4ee0fc18e85e811372a5c53a72ff
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-20 14:46:36 +1200

    fsdf

--- register.js
+++ register.js
@@ -53,9 +53,11 @@ point : req.body.point
 query = client.query('SELECT POINTS_LVL [$1] AS points, COUNT(username)  FROM RANK WHERE username = $2 GROUP BY POINTS_LVL[$1]',[req.params.lvl, obj.username]);
 var returnPoint = 0  ; var  p = -1 ; 
 query.on('row', function (result){
+if (result) {
 returnPoint = result.count ;
-p  = result.points ; 
-if( returnPoint !=0 ) return res.sendStatus(result.points) ; 
+p  = result.points ;
+if (returnPoint == 0 ) {res.statusCode =404 ;console.log ("NO"); res.send('no'); }
+}else { res.send(result.count) ; } 
 });
 
 query.on('end', function(){
