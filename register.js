8142454d1b7c81425eadff8daf70a7d7c274662f
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-06-21 15:15:38 +1200

    use index

--- register.js
+++ register.js
@@ -32,7 +32,7 @@ app.use(express.static(__dirname));
 app.use(cors());
 
 app.get('/' , function(req, res){
-     res.sendfile('index2.html');
+     res.sendfile('index.html');
 
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////
